283380d0990935431b8619c0fba865098964dd90
Giovanni-A18GitHub <awongiovanni18@gmail.comGitHub>
2025-03-05 09:27:00 +0100

    r commit

---
+++ acceuil.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FCT</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+              <p><span>Claudine</span>Talon</p>
+        </div>
+        <ul class="menu">
+            <li><a href="#">Acceuil</a></li>
+            <li><a href="#">Programme</a></li>
+            <li><a href="#">Actualité</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">A Propos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </header>
+
+    <!-- Conteneur pour l'image -->
+    <div class="image-container">
+        <img src="IMG_9696.PNG" alt="Image" class="image"> <!-- Remplacez votre-image.jpg par le chemin de votre image -->
+        <div class="text-overlay">
+            <p>Apporter une réponse rapide et intégrée aux besoins prioritaires des femmes et des enfants vulnérables au Bénin.</p>
+        </div>
+    </div>
+
+<!-- Lien Programme juste en bas de l'image -->
+<div class="programme-link">
+    <a href="#">Programmes</a>
+</div>
+
+<div class="additional-links">
+    <!-- Lien Education -->
+    <div class="link-item">
+        <a href="#">Education</a>
+        <p class="definition">Une brève définition sur l'éducation, pour expliquer ce que cela implique.</p>
+    </div>
+    <!-- Lien WASH -->
+    <div class="link-item">
+        <a href="#">WASH</a>
+        <p class="definition">WASH désigne l'Accès à l'eau potable, l'assainissement et l'hygiène.</p>
+    </div>
+    <!-- Lien Santé -->
+    <div class="link-item">
+        <a href="#">Santé</a>
+        <p class="definition">Des informations sur les services de santé et leur impact sur la population.</p>
+    </div>
+    <!-- Lien Automatisation -->
+    <div class="link-item">
+        <a href="#">Automatisation</a>
+        <p class="definition">L'automatisation dans le domaine de l'informatique et de la gestion.</p>
+    </div>
+    <!-- Lien Solidarité -->
+    <div class="link-item">
+        <a href="#">Solidarité</a>
+        <p class="definition">Un lien de soutien et de coopération entre les individus et les communautés.</p>
+    </div>
+</div>
+<div class="programme-link">
+    <a href="#">Actualités</a>
+</div>
+
+<!-- Section des 4 images -->
+<div class="image-section">
+    <div class="image-item">
+        <a href="#"><img src="IMG_0000.png" alt="Description1"></a>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias consequuntur tempora ut beatae,</p>
+    </div>
+    <div class="image-item">
+        <a href="#"><img src="IMG_1064.jpeg" alt="Description2"></a>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias consequuntur tempora ut beatae 2</p>
+    </div>
+    <div class="image-item">
+        <a href="#"><img src="IMG_9696.PNG" alt="Description3"></a>
+        <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias consequuntur tempora ut beatae3</p>
+    </div>
+    <div class="image-item">
+        <a href="#"><img src="IMG_0001.png" alt="Description4"></a>
+        <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias consequuntur tempora ut beatae 4</p>
+    </div>
+</div>
+<!-- Bouton Plus d'actualité -->
+<div class="more-news">
+    <input type="button" value="Plus d'actualité">
+</div>
+<!-- Footer -->
+<footer>
+    <div class="footer-container">
+        <!-- Colonne gauche : Menu -->
+        <div class="footer-menu">
+            <h3>Menu</h3>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Programme</a></li>
+                <li><a href="#">Actualité</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">À Propos</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+        <!-- Colonne centre : Contact et Réseaux sociaux -->
+        <div class="footer-contact">
+            <h3>Contact</h3>
+            <p>Email : contact@exemple.com</p>
+            <p>Téléphone : +229 123 456 789</p>
+            <h3>Suivez-nous</h3>
+            <div class="social-icons">
+                <a href="#"><img src="facebook.png" alt="Facebook"></a>
+                <a href="#"><img src="twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="instagram.png" alt="Instagram"></a>
+            </div>
+        </div>
+
+        <!-- Colonne droite : Logo -->
+        <div class="logo">
+              <p><span>Claudine</span>Talon</p>
+        </div>
+        </div>
+    </div>
+</footer>
+
+
+</body>
+</html>
